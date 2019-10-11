1ccc1fb0ded92615a8eda4bd772a1153b715e0ff
brenomendess <brenoowm@gmail.com>
2019-10-11 02:35:27 -0300

    Updated validation

--- gas_metering/gas_metering.ino
+++ gas_metering/gas_metering.ino
@@ -1,26 +1,55 @@
-const int reed_switch = 2;
+#include <TimerOne.h>
+#include <WISOL.h>
+#include <Tsensors.h>
+#include <Wire.h>
+#include <math.h>
+Isigfox *Isigfox = new WISOL();
+Tsensors *tSensors = new Tsensors();
 
+const int buttonPin = A1;
 int pulses = 0;
 float m3 = 0.00;
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(reed_switch, INPUT);
-
+// Init function
+void setup() {
+  Wire.begin();
+  Wire.setClock(100000);
+  Serial.begin(9600);// Init serial connection between Arduino and Modem
+  Isigfox->initSigfox();// WISOL modem test
+  Isigfox->testComms();
+  tSensors->initSensors();// Init sensors on Thinxtra Module
+  tSensors->setButton(buttonIR); // Init an interruption on the button of the Xkit
+  // pinMode(buttonPin, INPUT);
+  Timer1.initialize(2000000000); // Inicializa o Timer1 e configura para um período de X segundos
+  Timer1.attachInterrupt(send_data); // Configura a função Send_Pload() como a função para ser chamada a cada interrupção do Timer1
 }
 
-void loop()
-{
-  int reed_swicthState = digitalRead(reed_switch);
-
-  
-  if(reed_swicthState == HIGH){
+// Infinite loop of the program
+void loop() {
+  /*int buttonPinState = digitalRead(buttonPin);
+  if(buttonPinState == HIGH){
     pulses++;
   }
   m3 = pulses*0.01;
-  Serial.print("Consumo: ");
-  Serial.print(m3);
-  Serial.print(" m3\n");
-  delay(100);
- }
+  */
+  Serial.print("Consumo (m3): "); Serial.println(m3);
+  delay(1000);
+}
+
+// SendPayload Function => Send messages to the Sigfox Network
+void Send_Pload(uint8_t *sendData, int len) {
+  recvMsg *RecvMsg;
+  RecvMsg = (recvMsg *)malloc(sizeof(recvMsg));
+  Isigfox->sendPayload(sendData, len, 0, RecvMsg);
+  free(RecvMsg);
+}
+
+void send_data(){
+  Send_Pload((const char*)&m3, sizeof(m3));
+  Serial.println("The data was sent");
+}
+
+void buttonIR(){
+    pulses++;
+    m3 = pulses*0.01;
+}
