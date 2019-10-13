37ffd3165963bf2da5b0d49a1140b9f36ea719a7
brenomendess <brenoowm@gmail.com>
2019-10-13 20:29:36 -0300

    Fixed timer, now using millis()

--- gas_metering/gas_metering.ino
+++ gas_metering/gas_metering.ino
@@ -8,7 +8,8 @@ Tsensors *tSensors = new Tsensors();
 const int buttonPin = A1;
 int pulses = 0;
 float m3 = 0.00;
-
+unsigned long time = 0;
+int period = 20000;
 
 // Init function
 void setup() {
@@ -17,9 +18,7 @@ void setup() {
   Isigfox->testComms();
   tSensors->initSensors();// Init sensors on Thinxtra Module
   tSensors->setButton(buttonIR); // Init an interruption on the button of the Xkit
-  // pinMode(buttonPin, INPUT);
-  Timer1.initialize(3000000000); // Inicializa o Timer1 e configura para um período de X segundos
-  Timer1.attachInterrupt(send_data); // Configura a função Send_Pload() como a função para ser chamada a cada interrupção do Timer1
+  // pinMode(PINO DO REED SWITCH, INPUT);
 }
 
 // Infinite loop of the program
@@ -30,8 +29,12 @@ void loop() {
   }
   m3 = pulses*0.01;
   */
-  Serial.print("Consumo (m3): "); Serial.println(m3);
-  delay(1000);
+  if(millis() > time + period){
+    time = millis();
+    Serial.print("Consumo (m3): "); Serial.println(m3);
+    Send_Pload((const char*)&m3, sizeof(m3));
+    Serial.println("The data was sent");
+  }
 }
 
 // SendPayload Function => Send messages to the Sigfox Network
@@ -40,11 +43,6 @@ void Send_Pload(uint8_t *sendData, int len) {
   RecvMsg = (recvMsg *)malloc(sizeof(recvMsg));
   Isigfox->sendPayload(sendData, len, 0, RecvMsg);
   free(RecvMsg);
-}
-
-void send_data(){
-  Send_Pload((const char*)&m3, sizeof(m3));
-  Serial.println("The data was sent");
 }
 
 void buttonIR(){
